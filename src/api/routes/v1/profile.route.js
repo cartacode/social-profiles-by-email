950b80a3cb68793e81dd05c1109264d99e3ecc60
it-avenger <simbawow8@gmail.com>
2019-09-16 07:52:26 -0400

    initialize project

---
+++ src/api/routes/v1/profile.route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const validate = require('express-validation');
+const controller = require('../../controllers/profile.controller');
+const router = express.Router();
+
+router.route('/')
+  .get(controller.list);
+
+
+module.exports = router;
